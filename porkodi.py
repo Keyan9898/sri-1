f09d609194128aa1daeb4fbacc224a9d448dc7d9
sriarunaravi <46441123+sriarunaravi@users.noreply.github.com>
2019-01-07 14:57:34 +0530

    Create porkodi.py

---
+++ porkodi.py
@@ -0,0 +1,7 @@
+s=int(input("enter the number"))
+if(s>0):
+    print("The given number is positive")
+if(s<0):
+    print("The given number is negative")
+if(s==0):
+    print("The given number is zero")
