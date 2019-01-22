4a79db60a5a1d05c5c18a59209eed9e048665022
sriarunaravi <46441123+sriarunaravi@users.noreply.github.com>
2019-01-22 22:37:26 +0530

    Create alphanumbers.py

---
+++ alphanumbers.py
@@ -0,0 +1,14 @@
+
+s=input()#input
+c=0
+d=0
+for i in range(len(s)):
+    if s[i].isdigit():
+        c=c+1
+    elif s[i].isalpha():
+        d=d+1
+if c>0 and d>0:
+    print("Yes")
+else:
+    print("No")
+#56.py
