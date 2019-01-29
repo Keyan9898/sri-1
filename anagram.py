0be1cfecc25cfdb4350ed6a1d4b276d52059bdc0
sriarunaravi <46441123+sriarunaravi@users.noreply.github.com>
2019-01-29 16:36:58 +0530

    Create anagram.py

---
+++ anagram.py
@@ -0,0 +1,15 @@
+s="kabali"
+a=int(input())
+l=[]
+d=0
+for i in range(0,a):
+    l.append(input())
+for j in l:
+    c=0
+    for k in j:
+        if(k in s):
+            if(j.count(k)==s.count(k)):
+                c=c+1
+    if(c==len(j)):
+       d=d+1
+print(d)
