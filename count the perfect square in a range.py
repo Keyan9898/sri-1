2d058bf159200ffd3382f05ed4f1ab72c11c1d30
sriarunaravi <46441123+sriarunaravi@users.noreply.github.com>
2019-01-30 15:39:19 +0530

    Update count the perfect square in a range.py

--- count the perfect square in a range.py
+++ count the perfect square in a range.py
@@ -1,10 +1,9 @@
 import math#import the math
 
 n,a = map(int,input().split())
-b=n*a
-i = int(math.sqrt(b))
 c=0
 for s in range(n,a):
-    if(b == i*i):
+    i = int(math.sqrt(s))
+    if(s == i*i):
         c=c+1
 print(c)
