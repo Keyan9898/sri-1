58474d6523aadb44c3b407ac02bf7daa8b057be0
sriarunaravi <46441123+sriarunaravi@users.noreply.github.com>
2019-03-19 21:24:56 +0530

    Create pangram.py

---
+++ pangram.py
@@ -0,0 +1,17 @@
+n=input()
+a={}
+for j in range(97,123):
+    a.update({chr(j):0})
+n=n.replace(" ","")
+n=n.lower()
+c=0
+for i in n:
+    s=n.count(i)
+    a.update({i:s})
+for x,y in a.items():
+    if y>=1:
+        c=c+1
+if c==26:
+    print("yes")
+else:
+    print("no")
