e13319dfddfc2b0cf2f8170752b8639d60340ede
sriarunaravi <46441123+sriarunaravi@users.noreply.github.com>
2019-03-19 21:34:54 +0530

    Create repeat more 1 hunter.py

---
+++ repeat more 1 hunter.py
@@ -0,0 +1,21 @@
+n=int(input())#input
+lis=list(map(int,input().split()))
+a=[]
+d={ }
+for i in lis:
+     s=lis.count(i)
+     d.update({i:s})
+     a.append(s)
+w=max(a)
+if w>1:
+    c=0
+    for x,y in d.items():
+        if c==0:
+            if y>1:
+                print(x,end="")
+                c+=1
+        else:
+            if y>1:
+                print("",x,end="")
+else:
+    print("unique")
