36fa2fadc55bf7b9b3d63e244eda23fe5a38a9a5
sriarunaravi <46441123+sriarunaravi@users.noreply.github.com>
2019-05-13 14:32:47 +0530

    Create odd_player.py

---
+++ odd_player.py
@@ -0,0 +1,12 @@
+n=int(input())#input
+val=1
+c=0
+while(val<=n):
+    if n%val==0:
+        if val%2==1:
+            if c==0:
+                print(val,end="")
+                c+=1
+            else:
+                print("",val,end="")
+    val=val+1
